94b85cca505b01c403e3e32c16cc793e04f6a5d6
Marta Grus <mrs_marta@o2.pl>
2019-09-30 15:07:12 +0200

    Updates

---
+++ precache-manifest.97f6c77765f624da54d30a1511a615dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602aea3388b2f8f537c9adc0e80ced3c",
+    "url": "/projekt-zaliczeniowy-react/index.html"
+  },
+  {
+    "revision": "c3432dcd131ca07cb79f",
+    "url": "/projekt-zaliczeniowy-react/static/css/main.877fef82.chunk.css"
+  },
+  {
+    "revision": "4498bcd9f864dfa20afd",
+    "url": "/projekt-zaliczeniowy-react/static/js/2.ae632358.chunk.js"
+  },
+  {
+    "revision": "c3432dcd131ca07cb79f",
+    "url": "/projekt-zaliczeniowy-react/static/js/main.315bedf1.chunk.js"
+  },
+  {
+    "revision": "07babedc7ba07fcb0670",
+    "url": "/projekt-zaliczeniowy-react/static/js/runtime~main.43cd30a4.js"
+  }
+]);
