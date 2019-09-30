f6cc4171c8ab5c654eaea0324fa7fe857f8e2d49
Marta Grus <mrs_marta@o2.pl>
2019-09-30 15:13:29 +0200

    final

--- src/Offer/Box/Box.js
+++ src/Offer/Box/Box.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import './Box.css';
 
 class Box extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
     displayNewText () {
         if(this.props.box.isNew) return <span>(nowość)</span>
